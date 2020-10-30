8e63587fe8ec6ad3da92cc0f7177d83ade90f07f
mu1616 <mu1616@naver.com>
2020-10-31 06:22:22 +0900

    4-2. setTimeout 넣어서 반응속도 체크
    반응속도 체크 로직 완성

--- 4. 반응속도체크/response-check.jsx
+++ 4. 반응속도체크/response-check.jsx
@@ -7,16 +7,55 @@ class ResponseCheck extends Component {
     result: [],
   };
 
+  timeout;
+  startTime;
+  endTime;
+
+  onClickScreen = (e) => {
+    const { state, message, result } = this.state;
+    if (state === 'waiting') {
+      this.setState({
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요',
+      });
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: 'now',
+          message: '지금 클릭'
+        });
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+
+    } else if (state === 'ready') { // 성급하게 클릭
+      clearTimeout(this.timeout);
+      this.setState({
+        state: 'waiting',
+        message: '너무 성급하시군요! 초록색이 된 후에 클릭하세요.',
+      });
+      
+    } else if(state === 'now') {
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          result: [...prevState.result, this.endTime - this.startTime],
+          message: '클릭해서 시작하세요',
+        };
+      });
+    }
+  }
+
   render() {
+    const { state, message, result } = this.state;
     return (
       <>
-        <div id='screen' className={this.state.state} onClick={this.onClickScreen}>
-            {this.state.message}
+        <div id='screen' className={state} onClick={this.onClickScreen}>
+            {message}
         </div>
         <div>
-          {this.state.result.length === 0
+          {result.length === 0
             ? null
-            : <div>평균시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms</div>
+            : <div>평균시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
           }
         </div>
       </>
